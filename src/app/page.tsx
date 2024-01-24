53fe7987c7deb880e37b3b47b4ce39704104f02f
demarcj <demarcjohnson@gmail.com>
2024-01-24 12:34:17 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,15 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      <img
+        className="logo"
+        src="logo.png" alt="" 
+      />
+      <img
+        className="hero"
+        src="hero.jpg" alt="" 
+      />
+      {/* <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
@@ -89,7 +97,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }

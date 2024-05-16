c18380a360451e2339f2c07db99890f4bfe38fe8
demarcj <demarcjohnson@gmail.com>
2024-05-16 22:59:40 +0700

    Added dynamic sitemap

--- src/_server/property_server.tsx
+++ src/_server/property_server.tsx
@@ -44,6 +44,20 @@ export const get_properties = async (amount: number = 20): Promise<{properties: 
   })
 }
 
+export const get_all_properties = async (): Promise<{properties: PropertyModel[]}> => {
+  return new Promise(async (res, rej) => {
+    try{
+      const q = query(collection(db, `properties`), orderBy(`created_at`, `desc`));
+      const querySnapshot = await getDocs(q);
+      let items: PropertyModel[] = [];
+      querySnapshot.forEach(item => items = [...items, {...item.data(), ref: item.id} as PropertyModel]);
+      res(JSON.parse(JSON.stringify({properties: items})));
+    } catch(e) {
+      console.error(e);
+    }
+  })
+}
+
 export const update_property = async (property: PropertyModel): Promise<boolean> => {
   return new Promise(async (res, rej) => {
     try{
